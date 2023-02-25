2935de3f44b81d955ca56aba902ac94db2d283db
Avazbek Olimov <84025676+Avazbek22@users.noreply.github.com>
2023-02-25 21:08:47 +0300

    My_First_Commit

---
+++ Volodya/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Volodya // Пространство имен (название проекта)
+{
+    internal class Program // Класс - ООП
+    {
+        public static void Main(string[] args) // Метод Маин - точка входа в программу
+        {
+            // string str = "Hello World";
+            Console.Write("Введите N: ");
+            byte n = Byte.Parse(Console.ReadLine());
+            Console.WriteLine((char)n);
+        }
+    }
+}
